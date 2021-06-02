f1a90c1ca048b9bc39ec4d658434cdaa7e2ba802
KaiwenBian <biankaiwen111@berkeley.edu>
2021-06-01 19:54:33 -0700

    fix: remove index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/css/index.css";
-import Catalog from "./Catalog";
-
-ReactDOM.render(<Catalog />, document.getElementById("root"));
