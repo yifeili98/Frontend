cacddc64fe3fa52f8d358df2c3a5c28eada75aba
KaiwenBian <biankaiwen111@berkeley.edu>
2021-06-05 06:32:55 -0700

    fix: course display bug

--- src/pages/catalog/Catalog.js
+++ src/pages/catalog/Catalog.js
@@ -1,16 +1,17 @@
-import React, { useState, useCallback } from "react";
-import Header from "../../components/Header";
+import React, { useState, useCallback, useEffect } from "react";
 import CoursesWrapper from "../../components/CoursesWrapper";
 import CourseItem from "../../components/CourseItem";
 import axios from "axios";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import Filter from "./Filter";
 import CatalogDescription from "../../components/CatalogDescription";
+import Loading from "../../components/Loading";
 
 function Catalog() {
   const [courses, setCourses] = useState([]);
   const [filtedCourses, setFiltedCourses] = useState([]);
   const [specificCourse, setSpecificCourse] = useState({});
+  const [filterInfo, setFilterInfo] = useState();
 
   String.prototype.splice = function (idx, rem, str) {
     return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
@@ -26,7 +27,7 @@ function Catalog() {
     var reverseIndex = keyWord.length;
     //Finds case for ++, change it to \+\+
     while (reverseIndex > 0) {
-      if (keyWord[reverseIndex] != "+") {
+      if (keyWord[reverseIndex] !== "+") {
         reverseIndex--;
       } else {
         keyWord = keyWord.splice(reverseIndex, 0, "\\");
@@ -48,6 +49,49 @@ function Catalog() {
     setFiltedCourses(filteredClasses);
   };
 
+  useEffect(async () => {
+    //do https requrest
+    const allCourses = [];
+    axios
+      .get(
+        "https://fhda-api-test.azurewebsites.net/course_list?year=2021&quarter=Summer"
+      )
+      .then((res) => {
+        localStorage.setItem(
+          //This string can be obtained from API later, so I hardcode it here
+          '{"campus":"DeAnza","quarter":"Summer","year":"2021"}',
+          JSON.stringify(res.data)
+        );
+        for (const key in res.data) {
+          const courseObj = res.data[key];
+          let course = {
+            key: parseInt(key),
+            courseName: courseObj.courseTitle,
+            courseNum: courseObj.courseNum,
+            unit: +courseObj.numCredit,
+          };
+          allCourses.push(course);
+        }
+        setCourses(allCourses);
+        setFiltedCourses(allCourses);
+        setFilterInfo({ campus: "DeAnza", quarter: "Summer", year: "2021" });
+        const cache_list = [
+          { campus: "DeAnza", quarter: "Summer", year: "2021" },
+        ];
+        localStorage.setItem("cache_list", JSON.stringify(cache_list));
+        localStorage.setItem(
+          '{"campus":"DeAnza","quarter":"Summer","year":"2021"}'.concat(
+            "cards"
+          ),
+          JSON.stringify(allCourses)
+        );
+      });
+  }, []);
+
+  const clearCardsHandler = () => {
+    setFiltedCourses([]);
+  };
+
   const filterHandler = (filter_key, filter_value) => {
     var allCourses = [];
     const cache = JSON.parse(localStorage.getItem(JSON.stringify(filter_key)));
@@ -62,15 +106,23 @@ function Catalog() {
         };
         allCourses.push(course);
       }
-      localStorage.setItem(JSON.stringify(filter_key), JSON.stringify(allCourses));
+      localStorage.setItem(
+        JSON.stringify(filter_key),
+        JSON.stringify(filter_value.data)
+      );
       var cache_list = JSON.parse(localStorage.getItem("cache_list"));
       cache_list.push(filter_key);
       localStorage.setItem("cache_list", JSON.stringify(cache_list));
+      localStorage.setItem(
+        JSON.stringify(filter_key).concat("cards"),
+        JSON.stringify(allCourses)
+      );
     } else {
-
-      allCourses = JSON.parse(localStorage.getItem(JSON.stringify(filter_key)));
+      allCourses = JSON.parse(
+        localStorage.getItem(JSON.stringify(filter_key).concat("cards"))
+      );
     }
-    
+    setFilterInfo(filter_key);
     setCourses(allCourses);
     setFiltedCourses(allCourses);
     setSpecificCourse({});
@@ -81,10 +133,35 @@ function Catalog() {
   };
 
   const clickCourseItemHandler = (key) => {
-    let courseString = localStorage.getItem(key);
-    let course = JSON.parse(courseString);
-    setSpecificCourse(course);
+    const allCourses = JSON.parse(
+      localStorage.getItem(JSON.stringify(filterInfo))
+    );
+    setSpecificCourse(allCourses[parseInt(key)]);
   };
+
+  const courseCardsContent =
+    filtedCourses.length === 0 ? (
+      <Loading
+        type="spinningBubbles"
+        color="#8f0505"
+        height="15%"
+        width="15%"
+        className={"loading"}
+      />
+    ) : (
+      filtedCourses.map((course) => {
+        return (
+          <CourseItem
+            key={course.key}
+            uid={course.key}
+            courseName={course.courseName}
+            courseNum={course.courseNum}
+            unit={course.unit}
+            handlerClick={clickCourseItemHandler}
+          />
+        );
+      })
+    );
 
   return (
     <React.Fragment>
@@ -94,22 +171,10 @@ function Catalog() {
             <Filter
               filterHandler={filterHandler}
               searchCourseHandler={searchCourseHandler}
+              clearCardsHandler={clearCardsHandler}
             />
           </CoursesWrapper>
-          <CoursesWrapper>
-            {filtedCourses.map((course) => {
-              return (
-                <CourseItem
-                  key={course.key}
-                  uid={course.key}
-                  courseName={course.courseName}
-                  courseNum={course.courseNum}
-                  unit={course.unit}
-                  handlerClick={clickCourseItemHandler}
-                />
-              );
-            })}
-          </CoursesWrapper>
+          <CoursesWrapper>{courseCardsContent}</CoursesWrapper>
           <CoursesWrapper>
             {Object.keys(specificCourse).length !== 0 && (
               <CatalogDescription course={specificCourse} />
--- src/pages/catalog/Filter.js
+++ src/pages/catalog/Filter.js
@@ -22,6 +22,7 @@ class Filter extends React.Component {
   }
 
   handleSelect(event) {
+    this.props.clearCardsHandler();
     var event = event.target.id;
     const selectKey = event.substring(0, event.indexOf(":"));
     const selectValue = event.substring(event.indexOf(":") + 1);
@@ -29,39 +30,42 @@ class Filter extends React.Component {
       {
         [`${selectKey}`]: selectValue,
       },
-      function() {
+      function () {
         var cache_list = localStorage.getItem("cache_list");
         if (cache_list) {
-          cache_list = JSON.parse(cache_list)
+          cache_list = JSON.parse(cache_list);
           if (cache_list.length > 8) {
             localStorage.removeItem(cache_list[0]);
             cache_list = cache_list.shift();
           }
         } else {
-          cache_list = []
-          localStorage.setItem("cache_list", JSON.stringify(cache_list))
+          cache_list = [];
+          localStorage.setItem("cache_list", JSON.stringify(cache_list));
         }
-        const cache = JSON.parse(localStorage.getItem(JSON.stringify(this.state)));
+        const cache = JSON.parse(
+          localStorage.getItem(JSON.stringify(this.state))
+        );
         if (cache) {
           this.props.filterHandler(this.state, cache);
         } else {
           axios
-              .get("https://fhda-api-test.azurewebsites.net/course_list", {
-                params: {
-                  year: this.state.year,
-                  quarter: this.state.quarter,
-                },
-              })
-              .then(
-                function (response) {
-                  //Perform action based on response
-                  this.props.filterHandler(this.state, response);
-                }.bind(this)
-              )
-              .catch(function (error) {
-                console.log(error);
-                //Perform action based on error
-              });
+            .get("https://fhda-api-test.azurewebsites.net/course_list", {
+              params: {
+                year: this.state.year,
+                quarter: this.state.quarter,
+              },
+            })
+            .then(
+              function (response) {
+                //Perform action based on response
+                console.log(response);
+                this.props.filterHandler(this.state, response);
+              }.bind(this)
+            )
+            .catch(function (error) {
+              console.log(error);
+              //Perform action based on error
+            });
         }
       }
     );
@@ -80,11 +84,10 @@ class Filter extends React.Component {
               <h5>Campus: </h5>
             </Col>
             <Col>
-              <DropdownButton
-                as={ButtonGroup}
-                title={this.state.campus}
-              >
-                <Dropdown.Item onClick={this.handleSelect} id="campus:DeAnza">DeAnza</Dropdown.Item>
+              <DropdownButton as={ButtonGroup} title={this.state.campus}>
+                <Dropdown.Item onClick={this.handleSelect} id="campus:DeAnza">
+                  DeAnza
+                </Dropdown.Item>
                 <Dropdown.Item onClick={this.handleSelect} id="campus:Foothill">
                   Foothill (Coming Soon)
                 </Dropdown.Item>
@@ -96,10 +99,7 @@ class Filter extends React.Component {
               <h5>Year: </h5>
             </Col>
             <Col>
-              <DropdownButton
-                as={ButtonGroup}
-                title={this.state.year}
-              >
+              <DropdownButton as={ButtonGroup} title={this.state.year}>
                 <Dropdown.Item onClick={this.handleSelect} id="year:2021">
                   2021 (Usable)
                 </Dropdown.Item>
@@ -114,14 +114,19 @@ class Filter extends React.Component {
               <h5>Quarter: </h5>
             </Col>
             <Col>
-              <DropdownButton
-                as={ButtonGroup}
-                title={this.state.quarter}
-              >
-                <Dropdown.Item onClick={this.handleSelect} id="quarter:Fall">Fall</Dropdown.Item>
-                <Dropdown.Item onClick={this.handleSelect} id="quarter:Winter">Winter</Dropdown.Item>
-                <Dropdown.Item onClick={this.handleSelect} id="quarter:Spring">Spring</Dropdown.Item>
-                <Dropdown.Item onClick={this.handleSelect} id="quarter:Summer">Summer</Dropdown.Item>
+              <DropdownButton as={ButtonGroup} title={this.state.quarter}>
+                <Dropdown.Item onClick={this.handleSelect} id="quarter:Fall">
+                  Fall
+                </Dropdown.Item>
+                <Dropdown.Item onClick={this.handleSelect} id="quarter:Winter">
+                  Winter
+                </Dropdown.Item>
+                <Dropdown.Item onClick={this.handleSelect} id="quarter:Spring">
+                  Spring
+                </Dropdown.Item>
+                <Dropdown.Item onClick={this.handleSelect} id="quarter:Summer">
+                  Summer
+                </Dropdown.Item>
               </DropdownButton>
             </Col>
           </Row>
