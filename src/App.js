98b1fa72495de28480ace1a9d96542327b398590
YiFei Li <yifeil@berkeley.edu`>
2021-06-04 19:09:35 -0700

    fix: change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home";
 import Catalog from "./pages/catalog/Catalog";
 import Story from "./pages/story/Story";
 import About from "./pages/about/About";
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./assets/css/App.css";
 import { Nav, Navbar } from "react-bootstrap";
 import da_logo from "./assets/pic/fhdalogo.jpg";
@@ -17,9 +17,9 @@ export default function App() {
         </Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/catalog">Catalog</Nav.Link>
-          <Nav.Link href="/story">Story</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/#/catalog">Catalog</Nav.Link>
+          <Nav.Link href="/#/story">Story</Nav.Link>
+          <Nav.Link href="/#/about">About</Nav.Link>
         </Nav>
       </Navbar>
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
